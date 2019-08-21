6c4d01554dd8f8609c5e1468b812f3b295001098
mikaelbeat <ryynanenphm@gmail.com>
2019-08-21 15:58:27 +0300

    Update

--- Python_Programming_Masterclass/Oop/Demo/Accounts.py
+++ Python_Programming_Masterclass/Oop/Demo/Accounts.py
@@ -9,31 +9,31 @@ class Account:
         return pytz.utc.localize(utc_time)
     
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print("Account created for " + self.name)
+        self._name = name
+        self._balance = balance
+        self._transaction_list = [(Account._current_time(), balance)]
+        print("Account created for " + self._name)
         
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self._balance += amount
             self.show_balance()
-            self.transaction_list.append((Account._current_time(), amount))
+            self._transaction_list.append((Account._current_time(), amount))
             
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            self.transaction_list.append((Account._current_time(), -amount))
+        if 0 < amount <= self._balance:
+            self._balance -= amount
+            self._transaction_list.append((Account._current_time(), -amount))
             self.show_balance()
         else:
             print("Error in withdraw!")
             self.show_balance()
             
     def show_balance(self):
-        print(f"Balance is {self.balance}$.")
+        print(f"Balance is {self._balance}$.")
         
     def show_transactions(self):
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
                 tran_type = "deposited"
             else:
@@ -45,3 +45,10 @@ customer = Account("Customer", 0)
 customer.deposit(1000)
 customer.withdraw(500)
 customer.show_transactions()
+
+print("\n*******************\n")
+
+customer2 = Account("Customer2", 800)
+customer2.deposit(100)
+customer2.withdraw(200)
+customer2.show_transactions()
