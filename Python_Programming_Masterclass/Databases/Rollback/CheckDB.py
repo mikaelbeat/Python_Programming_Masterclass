b1c9fa9123cf2a95467ad3df2558dfbd85d3608a
mikaelbeat <ryynanenphm@gmail.com>
2019-11-06 15:13:42 +0200

    Update

--- Python_Programming_Masterclass/Databases/Rollback/CheckDB.py
+++ Python_Programming_Masterclass/Databases/Rollback/CheckDB.py
@@ -1,9 +1,11 @@
 import sqlite3
+import pytz
 
 db = sqlite3.connect("accounts.sqlite", detect_types = sqlite3.PARSE_DECLTYPES)
 
 for row in db.execute("SELECT * FROM history"):
-    local_time = row[0]
-    print(f"{local_time} - {type(local_time)}")
+    utc_time = row[0]
+    local_time = pytz.utc.localize(utc_time).astimezone()
+    print(f"{utc_time} - {local_time}")
     
 db.close()
