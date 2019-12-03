8e29cd6a8e229a529fdb798c01c14c13afcceadd
ryynapet <ryynapet@WL360807.eu.tieto.com>
2019-12-03 15:56:00 +0200

    Update

---
+++ Python_Programming_Masterclass/Modules_and_functions/Turtle/Turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(150)
+turtle.left(250)
+turtle.forward(150)
+turtle.left(250)
+turtle.forward(150)
+turtle.forward(50)
+turtle.left(250)
+turtle.forward(50)
+turtle.circle(90)
+turtle.circle(150)
+
+turtle.done()
