1a7a9e954dbd3eb6adeb54d5caaae3715d837313
mikaelbeat <ryynanenphm@gmail.com>
2019-12-08 22:28:00 +0200

    Update

--- Python_Programming_Masterclass/Modules_and_functions/Dates_and_times/Using_timezones.py
+++ Python_Programming_Masterclass/Modules_and_functions/Dates_and_times/Using_timezones.py
@@ -8,3 +8,12 @@ utc_time = datetime.datetime.utcnow()
 
 print(f"The time in {country} is {local_time}")
 print(f"UTC time is {utc_time}.")
+
+
+print("\n********** All timezones **********\n")
+
+for x in pytz.all_timezones:
+    print(x)
+    
+for x in sorted(pytz.country_names):
+    print(x + ": " + pytz.country_names[x])
